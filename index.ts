f494989c6f97f8e9f4dc984d3dcfd9940e4320c6
BasavarajMuttagi <basavaraj2770@gmail.com>
2024-06-20 00:56:26 +0530

    test cors

--- index.ts
+++ index.ts
@@ -12,7 +12,11 @@ import { PaymentRouter } from "./src/routes/payment.route";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
 const PORT = process.env.PORT;
 export const SECRET = process.env.SECRET;
